e603ec48352c247935ec8f028b7556bd2e08fbfa
mohammadaghigh <mohammadaghigh@gmail.com>
2023-10-26 00:06:56 +0330

    add card widget for posts

--- lib/Widget/post_widget.dart
+++ lib/Widget/post_widget.dart
@@ -7,65 +7,69 @@ Widget getBlogPost(
     required String buyValue}) {
   return Column(
     children: [
-      ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Image(
-          image: AssetImage(
-            'images/$imageName',
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 10.0,
-      ),
-      Text(
-        title,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 17.0,
-        ),
-        textAlign: TextAlign.center,
-      ),
-      SizedBox(
-        height: 10.0,
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'فروش روی: $sellValue',
-            style: TextStyle(color: Colors.red),
-          ),
-          SizedBox(
-            width: 3.0,
-          ),
-          Icon(
-            Icons.sell,
-            color: Colors.red,
-            size: 16.0,
-          ),
-          SizedBox(
-            width: 12.0,
-          ),
-          Text(
-            'خرید روی: $buyValue',
-            style: TextStyle(color: Colors.green),
-          ),
-          SizedBox(
-            width: 3.0,
-          ),
-          Icon(
-            Icons.price_check,
-            color: Colors.green,
-            size: 20.0,
-          ),
-        ],
-      ),
-      Container(
-        width: 200.0,
-        child: Divider(
-          color: Colors.black,
-          thickness: 1.0,
+      Card(
+        margin: EdgeInsets.all(7.0),
+        elevation: 7.0,
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image(
+                image: AssetImage(
+                  'images/$imageName',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'فروش روی: $sellValue',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  SizedBox(
+                    width: 3.0,
+                  ),
+                  Icon(
+                    Icons.sell,
+                    color: Colors.red,
+                    size: 16.0,
+                  ),
+                  SizedBox(
+                    width: 12.0,
+                  ),
+                  Text(
+                    'خرید روی: $buyValue',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                  SizedBox(
+                    width: 3.0,
+                  ),
+                  Icon(
+                    Icons.price_check,
+                    color: Colors.green,
+                    size: 20.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     ],
